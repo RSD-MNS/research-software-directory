c55bf8cfdcb3165477eecca0e3f9a61163ea3329
Riccardo Bianchi <contervis@gmail.com>
2021-02-08 12:55:07 +0100

    WIP on look and feel

--- database/mongo.sh
+++ database/mongo.sh
@@ -8,7 +8,7 @@ sleep 20
 
 if mongo --quiet rsd --eval "db.getCollectionNames().length" | grep -E '^0\s?$' ; then
     echo "Mongo DB seems empty, populating from db-init"
-    mongorestore -d rsd /db-init
+    #mongorestore -d rsd /db-init
 fi
 
 wait $MONGOD_PID
